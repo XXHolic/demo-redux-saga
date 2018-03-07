ad7cae263f1d0fcfa09e67f5f88793d15ed2558d
thy_dell <972143891@qq.com>
2018-03-07 23:31:31 +0800

    feat: try emmiter

--- example-event-emitter2.js
+++ example-event-emitter2.js
@@ -2,12 +2,12 @@ var events = require('events');
 var eventEmitter = new events.EventEmitter();
 
 var listener1 = function listener1() {
-	console.info('监听器 listener1执行');
-}
+  console.info('监听器 listener1执行');
+};
 
 var listener2 = function listener2() {
-	console.info('监听器 listener2执行');
-}
+  console.info('监听器 listener2执行');
+};
 
 // 绑定connection事件
 eventEmitter.addListener('connection',listener1);
@@ -18,3 +18,13 @@ console.info(eventListeners + '个监听器监听连接事件');
 
 eventEmitter.emit('connection');
 
+// 移除监听器
+eventEmitter.removeListener('connection', listener1);
+console.info('listener1 不受监听');
+
+eventEmitter.emit('connection');
+
+eventListeners = events.EventEmitter.listenerCount(eventEmitter,'connection');
+console.info(eventListeners + '个监听器监听连接事件');
+
+console.info('task done');
