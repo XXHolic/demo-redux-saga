5010880ad95a962d6b378d73efc2f08721828237
thy_dell <972143891@qq.com>
2018-03-10 22:10:59 +0800

    feat: try route

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 var http = require('http');
+var url = require('url');
 
-http.createServer(function (request,response) {
-  response.writeHead(200, {'Content-Type': 'text-plain'});
-  response.end('Hello World\n');
-}).listen(8888);
+function start(route) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.info('reqquest for '+ pathname + ' receives');
 
-console.info('Server running at http://127.0.0.1:8888/');
+    route(pathname);
+
+    response.writeHead(200,{'Content-Type': 'text/plain'});
+    response.write('hello world');
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.info('server has started');
+}
+
+exports.start = start;
