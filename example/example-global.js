0957c8d14c6e8fdbf11b7f35817e8450ee64652d
thy_dell <972143891@qq.com>
2018-03-15 23:41:45 +0800

    feat: try process

--- example/example-global.js
+++ example/example-global.js
@@ -4,3 +4,22 @@ console.info('filename:'+__filename);
 console.info('direname:'+__dirname);
 
 console.timeEnd('countMark');
+
+process.on('exit',function (code) {
+  setTimeout(function () {
+    console.log('This code is never run')
+  },0);
+  console.log('退出码为:',code);
+});
+console.info('程序执行结束');
+
+// 输出到终端
+process.stdout.write('hello world!'+'\n');
+
+process.argv.forEach(function (val, index, array) {
+  console.log(index + ':' + val);
+});
+// 获取执行路径
+console.log('执行路径' + process.execPath);
+// 平台信息
+console.log('平台信息'+process.platform);
